70f79f3a5145a4e10d884cbd67f3dc9aa751305d
Stefania <rinaldi.stefania09@gmail.com>
2022-05-03 17:44:49 -0300

    test

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<h1>Sign In</h1>
+
+<form action="comprueba_login.php" method='post'>
+    <label>Usuario: </label>
+    <input type='text' name='usuario'> <br><br>
+    <label>Contraseña: </label>
+    <input type='password' name='contrasenia'> <br><br>
+    <input type='submit' name='enviar' value="Login">
+</form>
+
+
+
+</body>
+</html>
+
