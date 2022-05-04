82322354719fe45b77d28272ede4ccfb8481c2ca
Stefania <rinaldi.stefania09@gmail.com>
2022-05-04 14:21:18 -0300

    clase usuario

---
+++ Usuario.php
@@ -0,0 +1,15 @@
+<?php
+
+class Usuario{
+
+    private $nombreUsuario;
+
+    public function __construct($nombreUsuario){
+        $this->nombreUsuario = $nombreUsuario;
+    }
+
+    public function buscarPokemon($pokemon){
+
+    }
+
+}
