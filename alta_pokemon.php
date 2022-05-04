4c4b61d9cb1e90954705f23ef06f90074c160bdb
Stefania <rinaldi.stefania09@gmail.com>
2022-05-04 14:04:37 -0300

    .alta pokemon

---
+++ alta_pokemon.php
@@ -0,0 +1,30 @@
+<?php
+require("datos_conexion.php");
+
+$id = $_POST["idPokemon"];
+$nombrePokemon = $_POST["nombrePokemon"];
+$tipoPokemon = $_POST["tipoPokemon"];
+$descripcion = $_POST["descripcionPokemon"];
+$imagen = $_POST["imagenPokemon"];
+
+$conexion = new mysqli($db_host, $db_usuario, $db_pass , $db_nombre );
+
+if($conexion->connect_errno){
+    echo "No se pudo conectar con el servidor " . $conexion->connect_errno;
+    exit();
+}
+
+$conexion->set_charset("utf8");
+
+$consulta = "INSERT INTO pokemon (uid, name, url_img, description, idType) VALUES ($id, '$nombrePokemon', '$imagen', '$descripcion', $tipoPokemon)";
+
+if($conexion->query($consulta)){
+    echo "Se añadio el Pokemon";
+}
+
+//por si consulta falla o esta mal escrita
+if($conexion->errno){
+    die($conexion->errno);
+}
+
+$conexion->close();
---
+++ formulario_baja_pokemon.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h1>Eliminar un Pokemon a la Pokedex</h1>
+
+
+<form action="baja_pokemon.php" method='post'>
+    <label>Nombre Pokemon: </label>
+    <input type='text' name='nombrePokemon'> <br><br>
+    <input type='submit' name='enviar' value="Eliminar Pokemon">
+</form>
+
+
+
+</body>
+</html>
