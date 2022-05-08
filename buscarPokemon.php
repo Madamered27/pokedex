69649419542a0e8f43549d3dc909f6ddeae6e6a9
alexisjv <alexisverba.1995@gmail.com>
2022-05-08 15:06:49 -0300

    se agrego la vista a la busqueda de pokemom

--- buscarPokemon.php
+++ buscarPokemon.php
@@ -1,14 +1,35 @@
 <?php
 include_once("datos_conexion.php");
+include_once ("header.php");
 
 //datos traidos del form del pokemon a buscar
 $dato = $_POST["dato"];
 
 $consultarPokemon = mysqli_query($conexion, "SELECT p.*, t.description as typeDescription FROM pokemon p JOIN type t ON p.idType = t.id WHERE p.uid = '$dato' OR p.name = '$dato' OR t.description = '$dato'");
 
-foreach ($consultarPokemon as $pokemon){
-    echo $pokemon["uid"];
-    echo "<br>";
-}
-
 $conexion->close();
+?>
+
+<!-- Tabla -->
+<div class="pokedex">
+    <table>
+        <tr>
+            <th>Imagen</th>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Tipo</th>
+            <th>Descripción</th>
+        </tr>
+
+        <?php
+
+        foreach ($consultarPokemon as $pokemon) {
+            echo '<tr><td><a href="detalle_pokemon.php?pokemonId='.$pokemon["uid"].'"><img src="' . $pokemon['url_img'] . '" width="100" height="100" /></a></td>';
+            echo "<td>" . $pokemon["uid"] . "</td>";
+            echo "<td>" . $pokemon['name'] . "</td>";
+            echo '<td><img title="' . $pokemon['typeDescription'] . '" src="img/' . $pokemon['typeDescription'] . '.png" width="50" height="50" /></td>';
+            echo "<td>" . $pokemon['description'] . "</td></tr>";
+        } ?>
+
+    </table>
+</div>
